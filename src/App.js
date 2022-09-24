f9138219d30bf7d45c4ed0bb9dfa055d6aff7df6
josh gulledge <joshthedevgulledge@proton.me>
2022-09-24 11:10:07 -0500

    bringing in material ui and playing with its responsiveness

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "@mui/material/Button";
+import Header from "./Header";
+import Grid from "@mui/material/Grid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	function alertEvent() {
+		alert("Button worked");
+	}
+
+	function logEvent() {
+		console.log("Button still worked");
+	}
+
+	return (
+		<div>
+			<Header />
+
+			<Grid
+				container
+				spacing={4}
+				columns={{ xs: 2, md: 6, lg: 12 }}>
+				<Grid
+					item
+					xs={2}
+					md={2}
+					lg={2}>
+					<Button
+						onClick={alertEvent}
+						variant="contained"
+						color="secondary">
+						Button 1
+					</Button>
+				</Grid>
+
+				<Grid
+					item
+					xs={2}
+					md={2}
+					lg={2}>
+					<Button
+						onClick={logEvent}
+						variant="contained"
+						color="primary">
+						Button 2
+					</Button>
+				</Grid>
+
+				<Grid
+					item
+					xs={2}
+					md={2}
+					lg={2}>
+					<Button
+						onClick={logEvent}
+						variant="contained"
+						color="secondary">
+						Button 3
+					</Button>
+				</Grid>
+
+				<Grid
+					item
+					xs={2}
+					md={2}
+					lg={2}>
+					<Button
+						onClick={logEvent}
+						variant="contained"
+						color="primary">
+						Button 4
+					</Button>
+				</Grid>
+
+				<Grid
+					item
+					xs={2}
+					md={2}
+					lg={2}>
+					<Button
+						onClick={logEvent}
+						variant="contained"
+						color="secondary">
+						Button 5
+					</Button>
+				</Grid>
+
+				<Grid
+					item
+					xs={2}
+					md={2}
+					lg={2}>
+					<Button
+						onClick={logEvent}
+						variant="contained"
+						color="primary">
+						Button 6
+					</Button>
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
 
 export default App;
